96ca1d6fe684dc45d9e929669ba22913a9a5f6ed
nicktien007 <proad01@yahoo.com.tw>
2019-10-24 17:39:19 +0800

    新增2-3，型別轉換範例

--- Nick.Demo.Ex2_3/Program.cs
+++ Nick.Demo.Ex2_3/Program.cs
@@ -65,6 +65,69 @@ namespace Nick.Demo.Ex2_3
 
             Console.WriteLine($"【bool】 isHappy 是：{isHappy.ToString()}");
             Console.WriteLine($"【bool】 isOlder 是：{isOlder.ToString()}");
+
+
+            //型別轉換
+
+            Console.WriteLine("\n==================型別轉換-數字轉換==================");
+            //數字轉換
+            //明確轉換
+            Console.WriteLine("明確轉換");
+            double d1 = 3.14159265359;
+            Console.WriteLine($"d1={d1}");
+
+            //float f1 = d1;      //隱含轉換(大資料轉小資料，不可隱含轉換)，報錯!!
+            float f1 = (float)d1; //明確轉換(大資料轉小資料，精度損失)
+            Console.WriteLine($"f1={f1}");
+
+            //隱含轉換
+            Console.WriteLine("\n隱含轉換");
+            float f2 = 3.1415f;
+            Console.WriteLine($"f2={f2}");
+
+            double d2 = f2;         //隱含轉換
+            double d3 = (double)f2; //明確轉換
+
+            Console.WriteLine($"d2={d2}");
+            Console.WriteLine($"d3={d3}");
+
+
+            Console.WriteLine("\n文字轉數字");
+            //文字轉數字
+            string a = "70";
+            string b = "30";
+
+            Console.WriteLine($"【字串a】= {a}, 【字串b】 = {b}");
+            Console.WriteLine($"【字串a】+【字串b】 = {a + b}");
+
+            //將字串a、字串b，轉型數字
+            Console.WriteLine("\n將字串a、字串b，轉型數字");
+            int aParse = int.Parse(a);
+            int bParse = int.Parse(b);
+
+            Console.WriteLine($"【數字a】= {aParse}, 【數字b】 = {bParse}");
+            Console.WriteLine($"【數字a】+【數字b】 = {aParse + bParse}");
+
+            //文字轉數字，TryParse
+            Console.WriteLine("\n文字轉數字，調用 TryParse");
+            int aTryParse = 0;
+            bool isSuccess = int.TryParse(a, out  aTryParse);
+            if (isSuccess)
+            {
+                Console.WriteLine($"轉換成功，結果為{aTryParse}");
+            }
+            else
+            {
+                Console.WriteLine($"轉換失敗，請檢查資料來源");
+            }
+
+
+            Console.WriteLine("\n數字轉文字");
+            //數字轉文字
+            float pi = 3.14f;
+            string piString = pi.ToString();
+
+            Console.WriteLine($"【浮點數pi】= {pi}, 【字串pi】 = {piString}");
         }
     }
 }
