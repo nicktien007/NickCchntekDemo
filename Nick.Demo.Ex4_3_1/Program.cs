6de64869a1ce7c576f01ab9e576dffe8b3927cb5
nicktien007 <proad01@yahoo.com.tw>
2019-11-11 11:54:55 +0800

    新增第四章練習1及練習2答案

---
+++ Nick.Demo.Ex4_3_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Nick.Demo.Ex4_3_1
+{
+    /// <summary>
+    /// Ex4_3_1：練習1
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入您的體重(公斤)");
+            string weightStr = Console.ReadLine();
+
+            Console.WriteLine("請輸入您的身高(公尺)");
+            string heightStr = Console.ReadLine();
+
+            //調用double.Parse 轉型
+            double weight = double.Parse(weightStr);
+            double height = double.Parse(heightStr);
+
+            //BMI公式：BMI公式：體重[公斤] / 身高[公尺] * 身高[公尺]
+            double BMI = weight / (height * height);
+
+            if (BMI < 15)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，非常嚴重的體重不足");
+            }
+            else if (BMI >= 15 && BMI < 16)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，嚴重體重不足");
+            }
+            else if (BMI >= 16 && BMI < 18.5)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，體重過輕");
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，保持的很棒喔!!體重正常(健康體重)");
+            }
+            else if (BMI >= 25 && BMI < 30)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，體重過重");
+            }
+            else if (BMI >= 30 && BMI < 35)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，肥胖I級（中等肥胖）");
+            }
+            else if (BMI >= 35 && BMI < 40)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，肥胖II級（嚴重肥胖）");
+            }
+            else if (BMI >= 40)
+            {
+                Console.WriteLine($"您的BMI為：{BMI}，肥胖III級（非常嚴重肥胖）");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nick.Demo.Ex4_4_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Nick.Demo.Ex4_4_1
+{
+    /// <summary>
+    /// Ex4_4_1：練習2
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string level = "A+";
+
+
+            Console.WriteLine($"今年的績效考核為：{level}");
+
+            switch (level)
+            {
+                case "A+":
+                    Console.WriteLine("恭喜你!!今年度獎金 150% !!");
+                    break;
+                case "A":
+                    Console.WriteLine("表現不錯喔!!今年度獎金 120% !!");
+                    break;
+                case "B+":
+                    Console.WriteLine("感謝今年辛苦負出!!今年度獎金 100% !!");
+                    break;
+                case "B":
+                    Console.WriteLine("加油!!今年度獎金 80% !!");
+                    break;
+                case "C":
+                    Console.WriteLine("不好意思，今年公司營收不好，沒有獎金QQ~~");
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
