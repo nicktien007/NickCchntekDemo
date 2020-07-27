ab5975458b55fc959c1b12545b01b7c724639bf5
NickTien <trojanhorse0077@gmail.com>
2020-07-27 20:32:47 +0800

    新增ex2_2_1,ex2_3_1

---
+++ Nick.Demo.Ex2_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick.Demo.Ex2_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入Dcard討論區代碼以進行爬蟲 => 1：感情版，2：美食，3：有趣");
+
+            string code = Console.ReadLine();
+
+            Console.WriteLine($"\n您輸入的代碼為：{code}，即將為你進行爬蟲~~~");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Nick.Demo.Ex2_3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick.Demo.Ex2_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "女友生氣的樣子";
+            int cp = 218;
+            double weight = 61.34;
+            double height = 1.54;
+
+            Console.WriteLine($"寶可夢姓名：{name}\n" +
+                              $"cp:{cp}\n"+
+                              $"身高：{height}\n"+
+                              $"體重：{weight}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
