4e5b1460d9a13e1f128b4bf6be2ead9ff928b5aa
nick <trojanhorse0077@gmail.com>
2020-08-13 17:39:20 +0800

    add Ex5_1_2

---
+++ Nick.Demo.Ex5_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nick.Demo.Ex5_1_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string result = "";
+
+            for (int i = 1; i <= 9; i++)
+            {
+                result += $"{1}*{i}={(1 * i).ToString("00")} ";
+
+                result += "\n";
+            }
+
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
