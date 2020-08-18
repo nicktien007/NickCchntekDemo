75f1e85d07667a8fe0f2f2705e208bde7d33d07a
nick <trojanhorse0077@gmail.com>
2020-08-18 16:52:08 +0800

    update Nick.Demo.Ex5_1_2

--- Nick.Demo.Ex5_1_2/Program.cs
+++ Nick.Demo.Ex5_1_2/Program.cs
@@ -10,9 +10,9 @@ namespace Nick.Demo.Ex5_1_2
 
             for (int i = 1; i <= 9; i++)
             {
-                result += $"{1}*{i}={(1 * i).ToString("00")} ";
-
-                result += "\n";
+                int calcResult = 1 * i;
+                
+                result += $"{1}*{i}={calcResult.ToString("00")} \n";
             }
 
             Console.Write(result);
