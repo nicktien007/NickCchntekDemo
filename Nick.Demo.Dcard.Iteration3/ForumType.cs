dc69442247f5a62009d21e4a73485f3e50e8da41
nick <trojanhorse0077@gmail.com>
2020-08-22 01:35:58 +0800

    Nick.Demo.Dcard.Iteration3

---
+++ Nick.Demo.Dcard.Iteration3/ForumType.cs
@@ -0,0 +1,55 @@
+namespace Nick.Demo.Dcard.Iteration3
+{
+    public enum ForumType
+    {
+        /// <summary>
+        /// 感情版
+        /// </summary>
+        relationship = 1,
+        
+        /// <summary>
+        /// 女孩
+        /// </summary>
+        girl,
+        
+        /// <summary>
+        /// 心情
+        /// </summary>
+        mood,
+        
+        /// <summary>
+        /// 有趣
+        /// </summary>
+        funny,
+        
+        /// <summary>
+        /// 美妝
+        /// </summary>
+        makeup,
+        
+        /// <summary>
+        /// 追星
+        /// </summary>
+        entertainer,
+        
+        /// <summary>
+        /// 穿搭
+        /// </summary>
+        dressup,
+        
+        /// <summary>
+        /// 時事
+        /// </summary>
+        trending,
+        
+        /// <summary>
+        /// 美食
+        /// </summary>
+        food,
+        
+        /// <summary>
+        /// 工作
+        /// </summary>
+        job
+    }
+}
--- Nick.Demo.Dcard.Iteration3/Program.cs
+++ Nick.Demo.Dcard.Iteration3/Program.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Net.Http;
 
-namespace Nick.Demo.Dcard.Iteration2
+namespace Nick.Demo.Dcard.Iteration3
 {
     internal class Program
     {
@@ -21,6 +21,15 @@ namespace Nick.Demo.Dcard.Iteration2
             Console.WriteLine($"\n您輸入的代碼為：{code},討論區為：{(ForumType)typeCode}，爬取筆數：{limit}，即將為你進行爬蟲~~~\n");
 
             
+            //抽出一個方法，參數為typeCode(討論區代碼)，limit(爬取筆數)
+            var jsonString = GetDcardList(typeCode, limit);
+
+            Console.WriteLine($"爬到的內容為：{jsonString}");
+            Console.Read();
+        }
+
+        private static string GetDcardList(int typeCode, string limit)
+        {
             HttpClient httpClient = new HttpClient();
 
             //組裝討論區列表的網址
@@ -28,19 +37,17 @@ namespace Nick.Demo.Dcard.Iteration2
                          $"{(ForumType) typeCode}/posts" +
                          $"?popular=true" +
                          $"&limit={limit}";
-            
+
             //發送請求並取得回應內容
             var response = httpClient.GetAsync(url).Result;
 
             //檢查回應的伺服器狀態StatusCode是否是200 OK
-            if (response.StatusCode != HttpStatusCode.OK) 
+            if (response.StatusCode != HttpStatusCode.OK)
                 throw new Exception("抓不到資料，請稍後再試!!");
-            
+
             //讀取Content內容
             string jsonString = response.Content.ReadAsStringAsync().Result;
-
-            Console.WriteLine($"爬到的內容為：{jsonString}");
-            Console.Read();
+            return jsonString;
         }
     }
 }
